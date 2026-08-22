90298e4b6179af67e0b57c3c45f0693169011bee
Bichael <mpwilkinson007@gmail.com>
2026-08-22 14:23:08 +0100

    wet squelch  - fixed implementation of funcs stuff

--- inc/util/apply.h
+++ inc/util/apply.h
@@ -8,22 +8,143 @@ namespace TUPLE_UTIL_NAMESPACE::details {
     }
 }
 
-namespace TUPLE_UTIL_NAMESPACE {
-    template<typename Func, TUPLE_UTIL_CONTAINER tup=TUPLE_UTIL_DEFAULT_CONTAINER>
-	struct get_func_args;
-	template<class T, TUPLE_UTIL_CONTAINER tup, typename ... Dom_Ts>
-	struct get_func_args<void(T::*)(const Dom_Ts&...), tup> {
-		using type = tup<Dom_Ts...>;
-	};
-    template<TUPLE_UTIL_CONTAINER tup>
-	struct get_func_args_ {
-        template<typename Func> using type = get_func_args<Func, tup>;
+namespace TUPLE_UTIL_NAMESPACE::pred {
+    template<typename Func>
+    struct is_func { static constexpr bool value = false; };
+
+    template<typename Ret_T, typename ... Arg_Ts>
+    struct is_func<Ret_T(Arg_Ts...)> { static constexpr bool value = true; };
+
+    template<typename Ret_T, typename ... Arg_Ts>
+    struct is_func<Ret_T(&)(Arg_Ts...)> { static constexpr bool value = true; };
+
+    template<typename Ret_T, typename ... Arg_Ts>
+    struct is_func<Ret_T(*)(Arg_Ts...)> { static constexpr bool value = true; };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct is_func<Ret_T(Base_T::*)(Arg_Ts...)> { static constexpr bool value = true; };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct is_func<Ret_T(Base_T::*)(Arg_Ts...) const> { static constexpr bool value = true; };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct is_func<Ret_T(Base_T::*)(Arg_Ts...) &> { static constexpr bool value = true; };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct is_func<Ret_T(Base_T::*)(Arg_Ts...) const &> { static constexpr bool value = true; };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct is_func<Ret_T(Base_T::*)(Arg_Ts...) &&> { static constexpr bool value = true; };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct is_func<Ret_T(Base_T::*)(Arg_Ts...) const &&> { static constexpr bool value = true; };
+    
+    template<typename Func>
+    static constexpr bool is_func_v = is_func<Func>::value;   
+}
+
+namespace TUPLE_UTIL_NAMESPACE::func {
+    template<typename Func>
+    struct get_return;
+
+    template<typename Ret_T, typename ... Arg_Ts>
+    struct get_return<Ret_T(Arg_Ts...)> : std::type_identity<Ret_T> { };
+
+    template<typename Ret_T, typename ... Arg_Ts>
+    struct get_return<Ret_T(&)(Arg_Ts...)> : std::type_identity<Ret_T> { };
+
+    template<typename Ret_T, typename ... Arg_Ts>
+    struct get_return<Ret_T(*)(Arg_Ts...)> : std::type_identity<Ret_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_return<Ret_T(Base_T::*)(Arg_Ts...)> : std::type_identity<Ret_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_return<Ret_T(Base_T::*)(Arg_Ts...) const> : std::type_identity<Ret_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_return<Ret_T(Base_T::*)(Arg_Ts...) &> : std::type_identity<Ret_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_return<Ret_T(Base_T::*)(Arg_Ts...) const &> : std::type_identity<Ret_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_return<Ret_T(Base_T::*)(Arg_Ts...) &&> : std::type_identity<Ret_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_return<Ret_T(Base_T::*)(Arg_Ts...) const &&> : std::type_identity<Ret_T> { };
+    
+    template<typename Func>
+    using get_return_t = get_return<Func>::type;
+    
+
+
+    template<typename Func>
+    struct get_base;
+    
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_base<Ret_T(Base_T::*)(Arg_Ts...)> : std::type_identity<Base_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_base<Ret_T(Base_T::*)(Arg_Ts...) const> : std::type_identity<Base_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_base<Ret_T(Base_T::*)(Arg_Ts...) &> : std::type_identity<Base_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_base<Ret_T(Base_T::*)(Arg_Ts...) const &> : std::type_identity<Base_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_base<Ret_T(Base_T::*)(Arg_Ts...) &&> : std::type_identity<Base_T> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts>
+    struct get_base<Ret_T(Base_T::*)(Arg_Ts...) const &&> : std::type_identity<Base_T> { };
+
+    template<typename Func>
+    using get_base_t = get_base<Func>::type;
+
+
+
+    template<typename Func, TUPLE_UTIL_CONTAINER Tup=TUPLE_UTIL_DEFAULT_CONTAINER>
+	struct get_args;
+    
+    template<typename Ret_T, typename ... Arg_Ts, TUPLE_UTIL_CONTAINER Tup>
+    struct get_args<Ret_T(Arg_Ts...), Tup> : std::type_identity<Tup<Arg_Ts...>> { };
+
+    template<typename Ret_T, typename ... Arg_Ts, TUPLE_UTIL_CONTAINER Tup>
+    struct get_args<Ret_T(&)(Arg_Ts...), Tup> : std::type_identity<Tup<Arg_Ts...>> { };
+
+    template<typename Ret_T, typename ... Arg_Ts, TUPLE_UTIL_CONTAINER Tup>
+    struct get_args<Ret_T(*)(Arg_Ts...), Tup> : std::type_identity<Tup<Arg_Ts...>> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts, TUPLE_UTIL_CONTAINER Tup>
+    struct get_args<Ret_T(Base_T::*)(Arg_Ts...), Tup> : std::type_identity<Tup<Arg_Ts...>> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts, TUPLE_UTIL_CONTAINER Tup>
+    struct get_args<Ret_T(Base_T::*)(Arg_Ts...) const, Tup> : std::type_identity<Tup<Arg_Ts...>> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts, TUPLE_UTIL_CONTAINER Tup>
+    struct get_args<Ret_T(Base_T::*)(Arg_Ts...) &, Tup> : std::type_identity<Tup<Arg_Ts...>> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts, TUPLE_UTIL_CONTAINER Tup>
+    struct get_args<Ret_T(Base_T::*)(Arg_Ts...) const &, Tup> : std::type_identity<Tup<Arg_Ts...>> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts, TUPLE_UTIL_CONTAINER Tup>
+    struct get_args<Ret_T(Base_T::*)(Arg_Ts...) &&, Tup> : std::type_identity<Tup<Arg_Ts...>> { };
+
+    template<typename Base_T, typename Ret_T, typename ... Arg_Ts, TUPLE_UTIL_CONTAINER Tup>
+    struct get_args<Ret_T(Base_T::*)(Arg_Ts...) const &&, Tup> : std::type_identity<Tup<Arg_Ts...>> { };
+    
+    template<TUPLE_UTIL_CONTAINER Tup=TUPLE_UTIL_DEFAULT_CONTAINER>
+	struct get_args_ {
+        template<typename Func> using type = get_args<Func, Tup>;
     };
-    template<typename Func, TUPLE_UTIL_CONTAINER tup=TUPLE_UTIL_DEFAULT_CONTAINER>
-	using get_func_args_t = get_func_args<Func, tup>::type;
-	
 
+    template<typename Func, TUPLE_UTIL_CONTAINER Tup=TUPLE_UTIL_DEFAULT_CONTAINER>
+	using get_args_t = get_args<Func, Tup>::type;
+}
 
+namespace TUPLE_UTIL_NAMESPACE {
     template<typename Tup, typename Func_T, typename ... Arg_Ts> 
     inline constexpr decltype(auto) apply(Func_T&& func, Arg_Ts&& ... args) {
         return details::apply<Tup>(std::forward<Func_T>(func), std::make_index_sequence<count_v<Tup>>{}, std::forward<Arg_Ts>(args)...);
